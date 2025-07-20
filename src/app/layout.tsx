1e221025131cef6a2de1b87dccbd431d9a26b730
Irian Durian <durianirian@gmail.com>
2025-07-19 22:17:13 -0500

    Barebones header and footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+
 export const metadata = {
   title: "Portfolio Website",
   description: "A portfolio website showcasing my projects and skills.",
@@ -12,7 +13,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="font-sans">{children}</body>
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="">
+        <header className="bg-transparent">
+          <nav>
+            <ul className="flex space-x-4 text-white">
+              <li><a href="/home">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/skills">Skills</a></li>
+              <li><a href="/projects">Projects</a></li>           
+            </ul>
+          </nav>
+        </header>
+        {children}
+        <footer className="bg-transparent text-center p-4 flex justify-between  items-center">
+          <div>&copy; {new Date().getFullYear()} My Portfolio</div>
+          <div id="contact-Links" >
+            <nav>
+              <ul>
+                <li><a href="https://www.linkedin.com/in/iriandurian/">LinkedIn</a></li>
+                <li><a href="https://github.com/saucypilot"></a>GitHub</li>
+                <li><a href="https://codepen.io/saucypilot"></a>CodePen</li>
+                <li><a href="https://devpost.com/saucypilot?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav">Devpost</a></li>
+              </ul>
+            </nav>
+          </div>
+        </footer>
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
+      <h1>Hey, I'm Irian. Welcome to my website.</h1>
     </div>
   );
 }
