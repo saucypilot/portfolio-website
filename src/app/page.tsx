77125dda9b95fb8760935529426dfb93de2d1ca6
Irian <durianirian@gmail.com>
2025-07-22 20:43:38 -0500

    Fix the threejs camera control not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Room from "./components/threeJSLandingPage";
 
 export default function Home() {
   return (
-    <div className=" postion-relative touchAction-none pointer-events-none">
+    <div className=" postion-relative">
       <h1>Hey, I'm Irian. Welcome to my website.</h1>
       <Room />
     </div>
