cb9cea8343557009d57ceb7febe9ab804ccd8e40
Irian Durian <durianirian@gmail.com>
2025-08-04 01:23:56 -0500

    Created blank for all my links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
           <div id="logoText">"Header"</div>
           <nav>
             <ul id="footerLinks">
-              <li><a href="/home">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/skills">Skills</a></li>
-              <li><a href="/projects">Projects</a></li>           
+              <li><a href="/">Home</a></li>
+              <li><a href="/pages/about">About</a></li>
+              <li><a href="/pages/skills">Skills</a></li>
+              <li><a href="/pages/projects">Projects</a></li>           
             </ul>
           </nav>
           <ThemeToggle />
---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageName = () => {
+  return (
+    <main>
+      <h1>Hello, Next.js!</h1>
+      <p>Welcome to your about page.</p>
+    </main>
+  );
+};
+
+export default PageName;
