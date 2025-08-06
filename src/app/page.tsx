4ae5af3ece9b5449c458e32ae11cb7e329e5715c
Irian <durianirian@gmail.com>
2025-08-06 12:38:24 -0500

    Added the three.js element to my landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,21 @@ import "./styles/homePage.css";
 export default function Home() {
   return (
     <div id="landingPage">
-      <h1 id="firstHeading">
-        Hey, I'm Irian. Welcome to my website.
-      </h1>
-      <Image
-        src="/headshot.png"
-        alt="headshot"
-        width={500}
-        height={500}
-      />
+      <div id="firstSection">
+        <h1 id="firstHeading">
+          Hey, I'm Irian. Welcome to my website.
+        </h1>
+        <Image
+          src="/headshot.png"
+          alt="headshot"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div id="secondSection">
+        <h1 id="secondHeading">I am a software engineer with a focus on Front-End Web Development and Game Development.</h1>
+        <Room />
+      </div>
     </div>
   );
 }
